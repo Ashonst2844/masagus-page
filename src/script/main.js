e5bc9b2442e7c5526fa61eb9d0121174dddf7cfa
Masagus Ahmad Ramadhan <agusyantosugiyanto@gmail.com>
2026-03-22 01:47:45 +0700

    create navigation section

--- src/script/main.js
+++ src/script/main.js
@@ -1,16 +1,17 @@
+gsap.registerPlugin(ScrollTrigger)
+
 var tl = gsap.timeline();
 document.addEventListener("DOMContentLoaded", () => {
-    tl.to("nav", {
-        y: 20,
-        duration: 1,
-        opacity: 1,
-        ease: "power2.in",
+    tl.to('#nav', {
+        x:0,
+        opacity:1,
+        duration:1,
+        ease: "power2.in"
     })
-    tl.to("nav a", {
-        y:0,
-        duration: 1,
-        opacity: 1,
+    tl.to('#nav-link a', {
+        x:0,
+        duration:1,
         ease: "power2.in",
-        stagger:0.4,
-    }, "-=0.5");
+        stagger:0.2
+    }, "-=0.5")
 });
